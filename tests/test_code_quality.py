65f8d86a8fe86f39b34dbef2d1a77e99c1ce99df
hingebase <zcliu@pku.edu.cn>
2026-08-22 07:39:39 +0800

    ✅ Lint TOML files

--- tests/test_code_quality.py
+++ tests/test_code_quality.py
@@ -15,11 +15,13 @@
 """Type checking and linting."""
 
 import contextlib
+import importlib.metadata
 import os
 import pathlib
 import runpy
 import subprocess  # noqa: S404
 import sys
+import sysconfig
 from collections.abc import Generator
 from typing import NoReturn
 
@@ -83,6 +85,30 @@ def test_ruff(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr(sys, "argv", ["ruff", "check"])
     with _astral_context(monkeypatch):
         _run_module("ruff")
+
+
+@pytest.mark.skipif(
+    os.getenv("PIXI_PROJECT_NAME") == "kimnara"
+        and os.getenv("PIXI_ENVIRONMENT_NAME", "default") != "default",
+    reason="It's unnecessary to run Tombi for each Python environment",
+)
+def test_tombi() -> None:
+    """Linting with Tombi."""
+    tombi = "tombi" + sysconfig.get_config_var("EXE")
+    try:
+        dist = importlib.metadata.distribution("tombi")
+    except ModuleNotFoundError:
+        pass
+    else:
+        if paths := dist.files:
+            for p in paths:
+                if p.name == tombi:
+                    match dist.locate_file(p):
+                        case pathlib.Path() as tombi:
+                            break
+                        case _:
+                            break
+    subprocess.run([tombi, "lint"], check=True)  # ruff: ignore[subprocess-without-shell-equals-true]
 
 
 def _run_module(module_name: str) -> None:
